71b4d801cdeb8e2f67484593c388a04fc66586bf
nithyasree2002 <nithyasreejagatheesan@gmail.com>
2025-12-03 13:25:09 +0000

    Fix Vite config: rename .js to .jsx, add index.html to root, install react-router-dom, fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Home from "./Home";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
   return (
