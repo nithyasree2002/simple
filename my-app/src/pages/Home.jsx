5232aac1ff3c7fb211c7287da35a17cc59b284aa
nithyasree2002 <nithyasreejagatheesan@gmail.com>
2025-12-03 03:50:54 +0000

    Add login and home pages

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Home Page</h2>
+      <p>Welcome User!</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ my-app/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Login Page</h2>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+export default Login;
